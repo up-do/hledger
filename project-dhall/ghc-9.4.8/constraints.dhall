4724d3b15f6b0b0e76e061015f1d86214d562d1d
Phil de Joux <philderbeast@gmail.com>
2024-05-18 09:33:19 -0400

    Configure for ghc-9.4.8

---
+++ project-dhall/ghc-9.4.8/constraints.dhall
@@ -0,0 +1,6 @@
+[ { dep = "brick", ver = "2.3.1" }
+, { dep = "safe", ver = "0.3.21" }
+, { dep = "vty", ver = "6.2" }
+, { dep = "vty-crossplatform", ver = "0.4.0.0" }
+, { dep = "vty-unix", ver = "0.2.0.0" }
+]
--- project-versions.mk
+++ project-versions.mk
@@ -4,8 +4,10 @@ GHC_VERSION ?= 9.0.2
 STACKAGE_VERSION ?= lts-19.33
 
 # For the upgrade, pick a matching pair of ghc-version and stack resolver.
-GHC_UPGRADE ?= 9.2.8
-STACKAGE_UPGRADE ?= lts-20.26
+# GHC_UPGRADE ?= 9.2.8
+# STACKAGE_UPGRADE ?= lts-20.26
+GHC_UPGRADE ?= 9.4.8
+STACKAGE_UPGRADE ?= lts-21.25
 
 # Imports can be relative to the project or relative to importing file.
 # ImportRelative works with cabal-3.10 and is the default.
